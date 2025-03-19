f1da43fa522f5f8fda3b75aec1ba4161376c86a6
mcdos63 <klvgk@mail.ru>
2025-03-19 16:33:05 +0400

    new function

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import pygame
 import random
 
+# Класс для создания частиц
+class Particle:
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.size = random.randint(6, 10)  # Увеличен начальный размер частиц
+        self.speed_x = random.uniform(-0.5, 0.5)  # Уменьшена скорость по X
+        self.speed_y = random.uniform(-1, 0.25)  # Уменьшена скорость по Y
+
+    def update(self):
+        self.x += self.speed_x
+        self.y += self.speed_y
+        self.size -= 0.03  # Уменьшена скорость уменьшения размера
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), int(self.size))
+
+# Список для хранения частиц
+particles = []
+
 pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -17,8 +38,6 @@ target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(99, 255), random.randint(99, 255), random.randint(9, 255))
 
-
-
 running = True
 
 while running:
@@ -29,8 +48,18 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                for _ in range(10):
+                    particles.append(Particle(mouse_x, mouse_y, (255, 0, 0)))
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+    # Обновление и отрисовка частиц
+    for particle in particles[:]:
+        particle.update()
+        particle.draw(screen)
+
+    # Удаление частиц, размер которых стал меньше или равен 0
+    particles = [particle for particle in particles if particle.size > 0]
 
     screen.blit(target_image, (target_x, target_y))
     pygame.display.update()
