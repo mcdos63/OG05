dbff861fb6a65ff376afa46bfa81295dab301fbb
mcdos63 <klvgk@mail.ru>
2025-03-19 15:53:33 +0400

    add cycle cod

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Тир Game")
-icon = pygame.image.load("image/img_1.png")
+icon = pygame.image.load("image/img_2.png")
 pygame.display.set_icon(icon)
 target_image = pygame.image.load("image/img.png")
-target_width = 80
+target_width = 70
 target_height = 80
+target_image = pygame.transform.scale(target_image, (target_width, target_height))
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
@@ -21,12 +22,17 @@ color = (random.randint(99, 255), random.randint(99, 255), random.randint(9, 255
 running = True
 
 while running:
+    screen.fill(color)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
-
-
-
+    screen.blit(target_image, (target_x, target_y))
+    pygame.display.update()
 
 pygame.quit()
